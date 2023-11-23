d055be9794fce4a19ff20b410c02617a042c2ce5
John-Luca Mascali <Mascj670@Newschool.edu>
2023-11-23 13:17:20 -0500

    Moved index.html to public directory and updated server setup

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const faker = require('faker');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-// Generate and log a random name
-const randomName = faker.name.findName();
-console.log(`Random Name: ${randomName}`);
 
-// Generate and log a random address
-const randomAddress = faker.address.streetAddress();
-console.log(`Random Address: ${randomAddress}`);
+app.use(express.static('public')); 
 
-// Generate and log a random phone number
-const randomPhoneNumber = faker.phone.phoneNumber();
-console.log(`Random Phone Number: ${randomPhoneNumber}`);
 
-// Generate and log a random email address
-const randomEmail = faker.internet.email();
-console.log(`Random Email: ${randomEmail}`);
+app.get('/faker-data', (req, res) => {
+    const data = {
+        name: faker.name.findName(),
+        address: faker.address.streetAddress(),
+        phoneNumber: faker.phone.phoneNumber(),
+        email: faker.internet.email(),
+        jobTitle: faker.name.jobTitle(),
+        catchPhrase: faker.company.catchPhrase(),
+        company: faker.company.companyName()
+    };
+    res.json(data);
+});
 
-// Generate and log a random job title
-const randomJobTitle = faker.name.jobTitle();
-console.log(`Random Job Title: ${randomJobTitle}`);
 
-// Generate and log a random catch phrase
-const randomCatchPhrase = faker.company.catchPhrase();
-console.log(`Random Catch Phrase: ${randomCatchPhrase}`);
-
-// Generate and log a random company name
-const randomCompany = faker.company.companyName();
-console.log(`Random Company: ${randomCompany}`);
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
